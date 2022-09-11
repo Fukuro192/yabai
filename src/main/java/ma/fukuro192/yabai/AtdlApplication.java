f65196edaf971b6f02aa60ffcfe16bf12b6b6d9a
Fukuro192 <salah8elmes.2000@gmail.com>
2022-09-12 00:32:36 +0200

    added main project

---
+++ src/main/java/ma/fukuro192/yabai/AtdlApplication.java
@@ -0,0 +1,13 @@
+package ma.fukuro192.yabai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtdlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtdlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/fukuro192/yabai/AtdlApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.fukuro192.yabai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtdlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
