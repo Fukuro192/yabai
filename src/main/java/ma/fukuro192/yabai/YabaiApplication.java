2cc02c2888fb0d67d72e1935e3fb0a68ff3993d2
Fukuro192 <salah8elmes.2000@gmail.com>
2022-09-12 00:35:02 +0200

    changed file name to corresponding project name

--- src/main/java/ma/fukuro192/yabai/YabaiApplication.java
+++ src/main/java/ma/fukuro192/yabai/YabaiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AtdlApplication {
+public class YabaiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AtdlApplication.class, args);
+		SpringApplication.run(YabaiApplication.class, args);
 	}
 
 }
